5676d0e2664efeb2c205d7c1e3961f248ba82ebe
EduardoHenriqueFabini <128606132+EduardoHenriqueFabini@users.noreply.github.com>
2023-09-20 11:06:10 -0300

    Add files via upload

---
+++ 19.py
@@ -0,0 +1,46 @@
+"""exercicio 1
+
+valor_casa = float(input("Qual o valor da casa"))
+salario = float(input("qual o valor do teu salario"))
+ano = int(input("Em quantos anos tu vai pagar essa casa?"))
+prestacao = (dalario*30)/100)*anos
+emprestimo = salario - prestacao
+
+if emprestimo = prestacao:
+    print('emprestimo negado')
+else:
+print ('Emprestimo aprovado')
+
+
+Exercicio 2
+"""
+
+luxo = 150
+popular = 90
+
+carro = float(input("Qual o carro que sera usadado, popular ou de luxo"))
+dias = int(input("Por quantos dias foi usado o carro?"))
+km = float(input("Quantos km's foram percorridos com o carro"))
+
+if carro == "luxo" and km <= 100:
+    preco = (luxo * dias)*km
+    print("O valor a ser pago é de ", preco "com a quantia de ", dias "e os kms percorridos sao", km)
+
+
+elif carro == "luxo" and km > 100:
+    preco = (luxo * dias)*km
+    print("O valor a ser pago é de ", preco "com a quantia de ", dias "e os kms percorridos sao", km)
+
+
+elif carro == "popular" and km <= 100:
+    preco = (popular * dias)*km
+    print("O valor a ser pago é de ", preco "com a quantia de ", dias "e os kms percorridos sao", km)
+
+
+else:
+    preco = preco = (popular * dias)*km
+    print("O valor a ser pago é de ", preco "com a quantia de ", dias "e os kms percorridos sao", km)
+
+
+
+
